26488631651bfe265f1466729aef0cfd77986dec
marinalgarcia <marinalgarcia@gmail.com>
2022-10-18 00:15:34 -0300

    Nuevas mejoras

--- ejemplo/models.py
+++ ejemplo/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Familiar(models.Model):
-    nombre = models.CharField(max_length=100)
-    direccion = models.CharField(max_length=200)
-    numero_pasaporte = models.IntegerField()
+    nombre = models.CharField(max_length=20)
+    direccion = models.CharField(max_length=100)
+    numero_documento = models.IntegerField()
+    codigo_postal = models.IntegerField()
 
 def __str__(self):
-    return f"{self.nombre}, {self.numero_pasaporte}, {self.id}"
+    return f"{self.nombre}, {self.numero_documento}, {self.id}"

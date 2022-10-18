8fbc8af481b6b1c0e14833122236de22371e600c
marinalgarcia <marinalgarcia@gmail.com>
2022-10-17 22:47:42 -0300

    Actualizacion para el MVT con Familiares

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from ejemplo.models import Familiar
 
 def index(request):
     return render(request, "ejemplo/saludar.html")
+
+def monstrar_familiares(request):
+  lista_familiares = Familiar.objects.all()
+  return render(request, "ejemplo/familiares.html", {"lista_familiares": lista_familiares})
--- project/urls.py
+++ project/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ejemplo.views import index
+from ejemplo.views import index, monstrar_familiares
+#from ejemplo.views import index, index_dos, index_tres, monstrar_familiares
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('saludar/', index), # ESTA ES LA NUEVA FUNCTION
+    path('saludar/', index),
+    #path('saludar/<nombre>/<apellido>/', index_dos),
+    #path('mostrar-notas/', index_tres),
+    path('mi-familia/', monstrar_familiares), # nueva vista
 ]
